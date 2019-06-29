b4116961fdd1faddac345a1ccd26542340298d41
Michael Bergeron <michaelbergeron@Michaels-MacBook-Pro.local>
2019-06-29 16:08:25 -0500

    added a procfile

--- server/db.js
+++ server/db.js
@@ -6,6 +6,19 @@ const db = mysql.createConnection({
   database: 'expenses'
 });
 
+db.query(`create database expenses;
+use expenses;
+create table if not exist categories(
+  id serial,
+  name varchar(255)
+);
+create table if not exist expense(
+  id serial,
+  cat_id int,
+  expense varchar(255),
+  cost Dec(10, 2)
+);`)
+
 const getData = (cb) => {
   db.query('select c.name, e.expense, e.cost from expense e inner join categories c where e.cat_id = c.id;', (err, results) => {
     if (err){console.log(err)}
